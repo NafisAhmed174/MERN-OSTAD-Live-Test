77c8a76833be7b2fee4a48d094974b1c6aaa06ed
NafisAhmed174 <nafisahmed947@gmail.com>
2024-06-28 22:11:50 +0600

    created function 1

---
+++ index.js
@@ -0,0 +1,7 @@
+// 1) Create a function that takes voltage and current and returns the calculated power.  Ex- circuitPower(110, 3) ➞ 330
+
+function circuitPower(voltage , current){
+    return voltage * current;
+}
+
+console.log(circuitPower(110, 3));
