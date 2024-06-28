f9f96c21ef11f4c111f7169b66087ea146ab5123
NafisAhmed174 <nafisahmed947@gmail.com>
2024-06-28 22:17:25 +0600

    created 2nd function

--- index.js
+++ index.js
@@ -4,4 +4,20 @@ function circuitPower(voltage , current){
     return voltage * current;
 }
 
+
+// 2) Write a JavaScript function named ‘findMaxNumber’ that takes an array of numbers as an argument and returns the maximum number in the array. If the array is empty, the function should return null.
+
+function findMaxNumber(...ar){
+    if(ar.length == 0) return null;
+    
+    max = ar[0];
+    
+    for(i = 1; i < ar.length; i++){
+        if(max < ar[i]) mx = ar[i];
+    }
+    
+    return max;
+}
+
 console.log(circuitPower(110, 3));
+console.log(findMaxNumber([2, 4 ,1]));
