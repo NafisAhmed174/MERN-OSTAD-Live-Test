742e41e85b13211db67c529f89057fe14cf15179
NafisAhmed174 <nafisahmed947@gmail.com>
2024-06-28 22:24:16 +0600

    created function mergeArrays

--- index.js
+++ index.js
@@ -19,5 +19,20 @@ function findMaxNumber(ar){
     return max;
 }
 
+// Write a JavaScript function named ‘mergeArrays’ that takes two arrays as arguments and returns a new array.
+
+function mergeArrays(ar1, ar2){
+    ar3 = [];
+    for(i = 0; i < ar1.length; i++){
+        ar3.push(ar1[i]);
+    }
+
+    for(i = 0; i < ar2.length; i++){
+        ar3.push(ar2[i]);
+    }
+    return ar3;
+}
+
 console.log(circuitPower(110, 3));
 console.log(findMaxNumber([2, 4 ,1]));
+console.log(mergeArrays([1, 2, 3], [4, 5, 6, 7]));
